396c5bc4012db36e7c28d0e3cd184bd47af8a911
John Dahl <jdahl@interactivejohn.com>
2018-01-25 15:54:04 -0600

    Added Skeleton Css Framework

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './assets/styles/normalize.scss';
+import './assets/styles/skeleton.scss';
 import Headphones from './headphones';
 import registerServiceWorker from './registerServiceWorker';
 
